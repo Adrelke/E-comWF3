296ab841220feb085488a6e33db471e181aa07d8
dulcine8 <dulce.cesar263@gmail.com>
2018-06-20 16:41:53 +0200

    changement nav sur form_ajouter_article

--- admin/form_ajouter_article.php
+++ admin/form_ajouter_article.php
@@ -17,9 +17,18 @@
     <title>Contact</title>
 </head>
 <body>
-<?php
-include ('../header.php');
-?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="../accueil.php">Accueil<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav_item active">
+                <a class="nav-link" href="adminadduser.php?deconnexion">Deconnexion<span class="sr-only">(current)</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 <br>
 <br>
 
@@ -154,10 +163,7 @@ include ('../header.php');
 ?>
 <br>
 <br>
-<?php
 
-include ('../footer.php');
-?>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
