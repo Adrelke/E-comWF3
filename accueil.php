82c7e6810d23e907e2e27911ae93e3f32313cb46
Adrelke <31904051+Adrelke@users.noreply.github.com>
2018-06-20 10:58:55 +0200

    ajout carousel, modif css border

--- accueil.php
+++ accueil.php
@@ -17,7 +17,47 @@
   </head>
   <body>
     <?php include('header.php')?>
-
+    <section class="container-fluid my-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div id="carouselId" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            <li data-target="#carouselId" data-slide-to="0" class="active"></li>
+                            <li data-target="#carouselId" data-slide-to="1"></li>
+                            <li data-target="#carouselId" data-slide-to="2"></li>
+                            <li data-target="#carouselId" data-slide-to="2"></li>
+                        </ol>
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active">
+                                <img src="http://via.placeholder.com/1100x600" alt="First slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="http://via.placeholder.com/1100x600" alt="Second slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="http://via.placeholder.com/1100x600" alt="Third slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="http://via.placeholder.com/1100x600" alt="Third slide">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselId" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container-fluid">
+        <h1>P</h1>
+    </section>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
