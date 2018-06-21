d0a76cb99814314b06a8d9f1013fd87a4fcd2f80
Adrelke <31904051+Adrelke@users.noreply.github.com>
2018-06-21 09:19:53 +0200

    update nav admin + deco

--- admin/adminadduser.php
+++ admin/adminadduser.php
@@ -103,11 +103,25 @@ if(!empty($_SESSION)){
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="../accueil.php">Accueil<span class="sr-only">(current)</span></a>
+          <li class="nav-item active mr-3">
+            <a class="btn btn-primary" href="../accueil.php">Accueil  <span class="sr-only">(current)</span></a>
           </li>
+          <li class="nav-item active mr-3">
+            <div class="dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Options
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="adminadduser.php">Ajout utilisateur</a>
+                  <a class="dropdown-item" href="form_ajouter_article.php">Ajout/modif article</a>
+                  <a class="dropdown-item" href="#">Modif informations</a>
+                  <a class="dropdown-item" href="#">Messages</a>
+                </div>
+            </div>
+          </li>
+
           <li class="nav_item active">
-            <a class="nav-link" href="adminadduser.php?deconnexion">Deconnexion<span class="sr-only">(current)</a>
+            <a class="btn btn-danger" href="adminadduser.php?deconnexion">Deconnexion<span class="sr-only">(current)</a>
           </li>
         </ul>
       </div>
@@ -115,7 +129,7 @@ if(!empty($_SESSION)){
 
 
 
-    <div class="container">
+    <div class="container mt-5">
     <form method="POST">
       <div class="form-group">
         <label for="exampleInputPassword1">Pseudo</label>
@@ -147,7 +161,8 @@ if(!empty($_SESSION)){
     echo 'Erreur';
 }
 ?>
-
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
 
--- admin/form_ajouter_article.php
+++ admin/form_ajouter_article.php
@@ -1,6 +1,13 @@
 <?php
     include('../bdd.php');
+    
     session_start();
+
+    if(isset($_GET['deconnexion'])){
+        session_destroy();
+        header('Location: ../accueil.php');
+    
+    }
 ?>
 
 <!DOCTYPE html>
@@ -15,18 +22,32 @@
     <title>Contact</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="../accueil.php">Accueil<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav_item active">
-                <a class="nav-link" href="adminadduser.php?deconnexion">Deconnexion<span class="sr-only">(current)</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active mr-3">
+                <a class="btn btn-primary" href="../accueil.php">Accueil  <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item active mr-3">
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Options
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a class="dropdown-item" href="adminadduser.php">Ajout utilisateur</a>
+                      <a class="dropdown-item" href="form_ajouter_article.php">Ajout/modif article</a>
+                      <a class="dropdown-item" href="#">Modif informations</a>
+                      <a class="dropdown-item" href="#">Messages</a>
+                    </div>
+                </div>
+              </li>
+    
+              <li class="nav_item active">
+                <a class="btn btn-danger" href="form_ajouter_article.php?deconnexion">Deconnexion<span class="sr-only">(current)</a>
+              </li>
+            </ul>
+        </div>
+    </nav>
 <br>
 <br>
 
